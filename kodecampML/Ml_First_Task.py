677edd831fe37d376c7c065c39138198357afc3c
ANITAH <anitasisi851@gmail.com>
2025-11-02 07:49:11 +0300

    Add files via upload

---
+++ kodecampML/Ml_First_Task.py
@@ -0,0 +1,12 @@
+# Machine Learning Lifecycle
+
+
+# Here is the link to the document you requested:
+
+
+
+
+https://docs.google.com/document/d/1wY0260k_a-Qj1qquSAMIkp7uvi4J1OVZCt9caI6No1Q/edit?usp=sharing
+
+
+
